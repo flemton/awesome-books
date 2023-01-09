b7bc51c7c8a4f2afe01266dcb2bc667a992ec105
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-01-09 22:44:18 +0600

    created html  to see booklist using javascript

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
 const Books = [
-    {
-      title: '',
-      author: ''
-    },
-    {
-      title: '',
-      author: ''
-    },
-    {
-      title: '',
-      author: ''
-    },
-    {
-      title: '',
-      author: ''
-    },
-    {
-      title: '',
-      author: ''
-    }
-  ];
+  {
+    title: "",
+    author: "",
+  },
+  {
+    title: "",
+    author: "",
+  },
+  {
+    title: "",
+    author: "",
+  },
+  {
+    title: "",
+    author: "",
+  },
+  {
+    title: "",
+    author: "",
+  },
+];
+
+const bookList = document.querySelector(".book-list");
+
+Books.forEach((Books) => {
+  const book = `
+      <h5>
+      ${Books.title}
+      </h5>
+      <p>
+      ${Books.author}
+      </p>
+      <button onclick="removeBook(${Books.title}, ${Books.author})">Remove</button>
+      <hr>
+      `;
+  bookList.innerHTML += book;
+});
+
+document.bookList.insertBefore(bookList, document.bookList.children[0]);
+
