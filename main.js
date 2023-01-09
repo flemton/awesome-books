774a42889318d077e904678f0a3cc6cd9eb62b14
Alhassan Osman <oalhassan847@gmail.com>
2023-01-09 20:22:42 +0000

    complete add book button

--- main.js
+++ main.js
@@ -1,68 +1,53 @@
-const Books = [
-  {
-    title: "Harry potter 1",
-    author: "Hawkings",
-  },
-  {
-    title: "Principia de mathimatica",
-    author: "Isaac Newton",
-  },
-  {
-    title: "Book 3",
-    author: "Author 3",
-  },
-  {
-    title: "Book 4",
-    author: "Author 4",
-  },
-  {
-    title: "Book 5",
-    author: "Author",
-  },
-];
+const Books = JSON.parse(localStorage.getItem('Books')) || [];
 
-const storedBooks = localStorage.getItem('Books')
 
-if (storedBooks)
-{
-    Books = JSON.parse('storedBooks');
-}
-else
-{
-    const storeBooks = JSON.stringify(Books);
-    localStorage.setItem(Books, storeBooks);
-}
-
-const bookList = document.querySelector(".book-list");
+const bookList = document.createElement('section');
+bookList.className = 'book-list';
 
 Books.forEach((Books) => {
-  const book = `
-      <p>
-      ${Books.title}
-      </p>
-      <p>
-      ${Books.author}
-      </p>
-      <button onclick="removeBook(${Books.title}, ${Books.author})">Remove</button>
-      <hr>
-      `;
-  bookList.innerHTML += book;
+    const book = `
+        <p>
+        ${Books.title}
+        </p>
+        <p>
+        ${Books.author}
+        </p>
+        <button onclick="removeBook(${Books.title}, ${Books.author})">Remove</button>
+        <hr>
+        `;
+    bookList.innerHTML += book;
 });
 
-bookList.insertBefore(bookList, bookList.children[0]);
-
-const formTitle = document.querySelector('#title');
-
-const formAuthor = document.querySelector('#author');
+document.body.insertBefore(bookList, document.body.children[1]);
 
 const addButton = document.querySelector('#add-book');
 
-function addBook(title, author) {
-    console.log(title)
-    Books.push({title: title, author: author });
+function addBook() {
+    let formTitle = document.querySelector('#title');
+
+    let formAuthor = document.querySelector('#author');
+    if (!formAuthor.value || !formTitle.value)
+    {
+        return;
+    }
+
+    else
+    {
+        Books.push({title: formTitle.value, author: formAuthor.value });
+
+        localStorage.setItem('Books', JSON.stringify(Books));
+        formAuthor.value = '';
+        formTitle.value = '';
+    }
   }
 
+  function removeBook(title, author) {
+    for (let i = 0; i < Books.length; i++)
+        if (Books[i].title == title & Books[i].author == author)
+        {
+            Books.splice(i, 1);
+        }
+  }
 
+addButton.addEventListener('click', addBook);
 
-
-addButton.addEventListener('click', addBook(formTitle.value, formAuthor.value));
