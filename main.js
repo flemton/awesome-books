eda8fa9445575993a77156547f5037a988250adc
Alhassan Osman <oalhassan847@gmail.com>
2023-01-11 00:57:37 +0000

    style and fix linter errors

--- main.js
+++ main.js
@@ -1,60 +1,62 @@
 const Books = JSON.parse(localStorage.getItem('Books')) || [];
 const bookList = document.createElement('section');
 bookList.className = 'book-list';
+let bookIndex = 0;
 
 Books.forEach((Books) => {
+  let bookClass = 'bookContainer';
+  if (bookIndex % 2 === 1) {
+    bookClass = 'bookContainer1';
+  }
   const book = `
-    <div class="bookContainer">
+    <div class="${bookClass}">
         <p>
-        ${Books.title} By ${Books.author}
+        ${Books.title} by ${Books.author}
         </p>
         <button class="remove">Remove</button>
     </div>`;
   bookList.innerHTML += book;
+  bookIndex += 1;
 });
 
 document.body.insertBefore(bookList, document.body.children[1]);
 
 const addButton = document.querySelector('#add-book');
 const removeButton = document.querySelectorAll('.remove');
-console.log(removeButton)
 
 class Book {
-  constructor(title,author){
-    this.title =title;
+  constructor(title, author) {
+    this.title = title;
     this.author = author;
   }
+
   static addBook() {
-  const formTitle = document.querySelector('#title');
+    const formTitle = document.querySelector('#title');
 
-  const formAuthor = document.querySelector('#author');
-  console.log(formAuthor)
-  const bbook = new Book(formTitle.value,formAuthor.value)
-  console.log(bbook)
-  if (formAuthor.value && formTitle.value) {
-    Books.push(bbook);
+    const formAuthor = document.querySelector('#author');
+    const bbook = new Book(formTitle.value, formAuthor.value);
+    if (formAuthor.value && formTitle.value) {
+      Books.push(bbook);
 
-    localStorage.setItem('Books', JSON.stringify(Books));
-    formAuthor.value = '';
-    formTitle.value = '';
-    window.location.reload();
+      localStorage.setItem('Books', JSON.stringify(Books));
+      formAuthor.value = '';
+      formTitle.value = '';
+      window.location.reload();
+    }
+  }
+
+  static removeBook(e) {
+    const bookTitle = (e.target.parentNode.children[0].innerHTML).split('by')[0].trim();
+    const bookAuthor = (e.target.parentNode.children[0].innerHTML).split('by')[1].trim();
+    for (let i = 0; i < Books.length; i += 1) {
+      if (Books[i].title === bookTitle && Books[i].author === bookAuthor) {
+        Books.splice(i, 1);
+        localStorage.setItem('Books', JSON.stringify(Books));
+        window.location.reload();
+        break;
+      }
+    }
   }
 }
-
-static removeBook(e){
-  const bookTitle=(e.target.parentNode.children[0].innerHTML).split('By')[0].trim()
-  const bookAuthor =(e.target.parentNode.children[0].innerHTML).split('By')[1].trim()
-  for(let i=0;i<Books.length;i++){
-  {if (Books[i].title == bookTitle && Books[i].author == bookAuthor){
-Books.splice(i,1);
-localStorage.setItem('Books', JSON.stringify(Books));
-  window.location.reload();
-  break;
-  }}
-}
-}
-
-}
 addButton.addEventListener('click', Book.addBook);
-removeButton.forEach((element, i) => element.addEventListener('click', Book.removeBook,))
-
+removeButton.forEach((element) => element.addEventListener('click', Book.removeBook));
