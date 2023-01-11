11b97293f7a00d101f5071c25365be8501b8b748
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-01-12 04:14:21 +0600

    added evenlistener at global scope to switch between three section

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-
+window.addEventListener('DOMContentLoaded', () => {
+  const booksLink = document.querySelector('.list');
+  const formsLink = document.querySelector('.add-new');
+  const contactsLink = document.querySelector('.contact-link');
+  const booksPage = document.querySelector('.book-list');
+  const formsPage = document.querySelector('.form');
+  const contactsPage = document.querySelector('.contact');
+  const booksTitle = document.querySelector('.books-head');
+  formsPage.classList.add('hide');
+  contactsPage.classList.add('hide');
+  function showListOnly() {
+    formsPage.classList.add('hide');
+    contactsPage.classList.add('hide');
+    booksPage.classList.remove('hide');
+    booksTitle.classList.remove('hide');
+  }
+  booksLink.addEventListener('click', showListOnly);
+  function showContactOnly() {
+    booksPage.classList.add('hide');
+    formsPage.classList.add('hide');
+    booksTitle.classList.add('hide');
+    contactsPage.classList.remove('hide');
+  }
+  contactsLink.addEventListener('click', showContactOnly);
+  function showFormOnly() {
+    booksPage.classList.add('hide');
+    contactsPage.classList.add('hide');
+    booksTitle.classList.add('hide');
+    formsPage.classList.remove('hide');
+  }
+  formsLink.addEventListener('click', showFormOnly);
+});
 const Books = JSON.parse(localStorage.getItem('Books')) || [];
 const bookList = document.createElement('section');
 bookList.className = 'book-list';
