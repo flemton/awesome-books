2eb0770e803568952a9e4bef3e1283f1b2e3af6d
Alhassan Osman <oalhassan847@gmail.com>
2023-01-15 03:09:26 +0000

    fix #2 comment by SFarida

--- main.js
+++ main.js
@@ -70,26 +70,22 @@ class Book {
 
     const formAuthor = document.querySelector('#author');
     const bbook = new Book(formTitle.value, formAuthor.value);
-    if (formAuthor.value && formTitle.value) {
       Books.push(bbook);
 
       localStorage.setItem('Books', JSON.stringify(Books));
       formAuthor.value = '';
       formTitle.value = '';
       window.location.reload();
-    }
   }
 
   static removeBook(e) {
     const bookTitle = (e.target.parentNode.children[0].innerHTML).split('by')[0].trim();
     const bookAuthor = (e.target.parentNode.children[0].innerHTML).split('by')[1].trim();
     for (let i = 0; i < Books.length; i += 1) {
-      if (Books[i].title === bookTitle && Books[i].author === bookAuthor) {
         Books.splice(i, 1);
         localStorage.setItem('Books', JSON.stringify(Books));
         window.location.reload();
         break;
-      }
     }
   }
 }
